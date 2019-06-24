054da1b6ad26443cf0121abfb93175552fe3d693
Ruby Wolf <ruby.w@pm.me>
2019-06-24 20:09:16 +0000

    Fixed subdirectories being treated like files when importing.

--- tools/import.php
+++ tools/import.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 if (php_sapi_name() !== 'cli') exit();
 
@@ -18,7 +18,7 @@ if (!$bulk_settings[0]) {
 $directories = $bulk_settings[2]->directories;
 $defaults    = $bulk_settings[2]->settings;
 
-if (!is_writable($directories->dir_source) || 
+if (!is_writable($directories->dir_source) ||
     !is_writable($directories->dir_failed) ||
     !is_writable($directories->dir_target)) {
   exit("One or more bulk import directories not writable by script.");
@@ -29,12 +29,16 @@ if (!is_writable(OB_ASSETS . '/uploads')) {
 }
 
 foreach (new FilesystemIterator($directories->dir_source) as $file) {
+  if ($file->isDir()) {
+    continue;
+  }
+
   $src  = $file->getPathname();
   $fn = $file->getFilename();
   $info = $media_model('media_info', $src);
   $expiry = time() + 86400;
   $key = bin2hex(openssl_random_pseudo_bytes(16));
-    
+
   $file_id = $db->insert('uploads', array(
     'key'      => $key,
     'expiry'   => $expiry,
@@ -42,20 +46,20 @@ foreach (new FilesystemIterator($directories->dir_source) as $file) {
     'format'   => $info['format'],
     'duration' => $info['duration']
   ));
-  
+
   if (!$file_id) {
     rename($src, $directories->dir_failed . "/" . $fn);
     cleanup_uploads($file_id);
     continue;
   }
-  
+
   $tgt = OB_ASSETS . '/uploads/' . $file_id;
   if (!copy($src, $tgt)) {
     rename($src, $directories->dir_failed . "/" . $fn);
     cleanup_uploads($file_id);
     continue;
   }
-  
+
   $item = array(
     'file_id'   => $file_id,
     'file_info' => $info,
@@ -65,7 +69,7 @@ foreach (new FilesystemIterator($directories->dir_source) as $file) {
   foreach ($defaults as $field => $value) {
     $item[$field] = $value;
   }
-  
+
   $valid = $media_model('validate', $item);
   if (!$valid[0]) {
     echo "Validation error: " . $valid[2] . "\n";
@@ -73,24 +77,24 @@ foreach (new FilesystemIterator($directories->dir_source) as $file) {
     cleanup_uploads($file_id);
     continue;
   }
-  
+
   $media_id = $media_model('save', $item);
   if (!$media_id) {
     rename($src, $directories->dir_failed . "/" . $fn);
     cleanup_uploads($file_id);
     continue;
   }
-  
+
   rename($src, $directories->dir_target . "/" . $fn);
   echo "Processed file: " . $fn . "\n";
   cleanup_uploads($file_id);
-  
+
 }
 
 function cleanup_uploads ($file_id) {
   $db = OBFDB::get_instance();
   $db->where('id', $file_id);
   $db->delete('uploads');
-  
+
   @unlink(OB_ASSETS . '/uploads/' . $file_id);
 }
